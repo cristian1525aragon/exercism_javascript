ac5cb78d68fad9d80ece2f01d5e5239429577ccf
Cristian Aragón <cristian2000aragon@gmail.com>
2021-05-23 15:48:09 -0500

    cuarto commit

---
+++ anagram/anagram.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+export const findAnagrams = (word, arr) => {
+  word = word.toLowerCase()
+  
+  function isAnagram(anagram) {
+    if (word.length !== anagram.length) return false
+    anagram = anagram.toLowerCase()
+    if (word === anagram) return false
+    for (let letter of word) {
+      anagram = anagram.replace(letter,"")
+    }
+    return !anagram 
+  } 
+  return arr.filter(isAnagram);
+};
